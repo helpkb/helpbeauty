c9b981b879f69ca927ccbfdbd1e7a00d97625263
Niels Drost <nielsdrost7@gmail.com>
2016-02-07 22:04:58 +0100

    Commit it all

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,34 +1,56 @@
 <?php
 
+
 // Blog pages
 get('/', function () {
-    return redirect('/blog');
+  return redirect('/blog');
 });
+
+
 get('blog', 'BlogController@index');
 get('blog/{slug}', 'BlogController@showPost');
+
+
 $router->get('contact', 'ContactController@showForm');
 Route::post('contact', 'ContactController@sendContactInfo');
+
+
 get('rss', 'BlogController@rss');
 get('sitemap.xml', 'BlogController@siteMap');
 
 // Admin area
 get('admin', function () {
-    return redirect('/admin/post');
+  return redirect('/admin/post');
 });
 $router->group([
-    'namespace' => 'Admin',
-    'middleware' => 'auth',
+  'namespace' => 'Admin',
+  'middleware' => 'auth',
 ], function () {
-    resource('admin/post', 'PostController', ['except' => 'show']);
-    resource('admin/tag', 'TagController', ['except' => 'show']);
-    get('admin/upload', 'UploadController@index');
-    post('admin/upload/file', 'UploadController@uploadFile');
-    delete('admin/upload/file', 'UploadController@deleteFile');
-    post('admin/upload/folder', 'UploadController@createFolder');
-    delete('admin/upload/folder', 'UploadController@deleteFolder');
+  resource('admin/post', 'PostController', ['except' => 'show']);
+  resource('admin/tag', 'TagController', ['except' => 'show']);
+  get('admin/upload', 'UploadController@index');
+  post('admin/upload/file', 'UploadController@uploadFile');
+  delete('admin/upload/file', 'UploadController@deleteFile');
+  post('admin/upload/folder', 'UploadController@createFolder');
+  delete('admin/upload/folder', 'UploadController@deleteFolder');
 });
 
+
+/*
 // Logging in and out
+get('/auth/register', 'Auth\AuthController@getRegister');
+post('/auth/register', 'Auth\AuthController@postRegister');
 get('/auth/login', 'Auth\AuthController@getLogin');
 post('/auth/login', 'Auth\AuthController@postLogin');
 get('/auth/logout', 'Auth\AuthController@getLogout');
+*/
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
